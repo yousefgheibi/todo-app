18b21de023ea21555a7199347f5602d0cbcc3367
Yousef <gheibiyousef2000@gmail.com>
2021-10-11 20:45:57 +0330

    add Light/Dark Demo

--- assets/script.js
+++ assets/script.js
@@ -34,3 +34,16 @@ document.querySelector('#push').onclick = function (){
     }
 
 }
+
+
+const toggleSwitch = document.querySelector('.toggle-button');
+
+function switchTheme(e) {
+    if(e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
